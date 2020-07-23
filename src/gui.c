13f5e98f395b16aa8e4f673c8490df005d8a1edc
Alexander Kulak <sa-dev@odd.systems>
2020-07-23 11:01:31 +0300

    update copyright year

--- src/gui.c
+++ src/gui.c
@@ -1,7 +1,7 @@
 /*
 Draw and interface with our switcher window.
 
-Copyright 2017-2019 Alexander Kulak.
+Copyright 2017-2020 Alexander Kulak.
 This file is part of alttab program.
 
 alttab is free software: you can redistribute it and/or modify
