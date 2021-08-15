6401d941db96395ef44789dd24b0a621b5240841
aleks <aleks.stier@icloud.com>
2021-08-15 17:23:40 +0200

    implement optional border around the alttab-window
    Add the ability to draw a border around the alttab-window by using the
    native X-window-border. The border can be customized with the following
    options from the commandline:
    
    - -border:  defines the border-color (default: black)
    - -borderw: defines the border-width (default: 0)

--- src/alttab.h
+++ src/alttab.h
@@ -36,6 +36,7 @@ along with alttab.  If not, see <http://www.gnu.org/licenses/>.
 #define DEFTILEH    128
 #define DEFICONW    32
 #define DEFICONH    32
+#define DEFBORDERW  0
 #define DEFTHEME    "hicolor"
 #define FRAME_W     8
 //#define DEFFONT   "xft:DejaVu Sans Condensed-10"
@@ -44,10 +45,12 @@ along with alttab.  If not, see <http://www.gnu.org/licenses/>.
 #define COLBG       0
 #define COLFG       1
 #define COLFRAME    2
-#define NCOLORS     3
+#define COLBORDER   3
+#define NCOLORS     4
 #define DEFCOLBG    "black"
 #define DEFCOLFG    "grey"
 #define DEFCOLFRAME "#a0abab"
+#define DEFCOLBORDER "black"
 
 #define XDEPTH      24          // TODO: get rid of this
 
@@ -152,6 +155,8 @@ typedef struct {
     char *option_font;
     int option_tileW, option_tileH;
     int option_iconW, option_iconH;
+#define BORDER_MIN      0
+    int option_borderW;
 #define VP_FOCUS        0
 #define VP_POINTER      1
 #define VP_TOTAL        2
